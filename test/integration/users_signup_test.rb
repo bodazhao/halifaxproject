2b91b9b3882f8ac959792e5f2556fbc77731c62c
bodazhao <bodazhao@hotmail.com>
2015-08-24 19:12:55 +0000

    flash error message test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,6 +11,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: "bar" }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
   end
   
   test "valid signup information" do
@@ -22,6 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not_nil flash
   end
   
 end
