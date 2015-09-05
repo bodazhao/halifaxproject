1030d5336ed942d8614877614246b01c02759611
bodazhao <bodazhao@hotmail.com>
2015-09-05 15:56:04 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,13 @@ root                  'static_pages#home'
   post 'login' =>     'sessions#create'
   delete 'logout' =>  'sessions#destroy'
   
-  resources :users
+  resources :users do
+    member do 
+      get :following, :followers
+    end
+  end
   resources :account_activations,   only: [:edit]
   resources :password_resets,       only: [:new, :create, :edit, :update]
   resources :microposts,            only: [:create, :destroy]
-
+  resources :relationships,         only: [:create, :destroy]
 end
